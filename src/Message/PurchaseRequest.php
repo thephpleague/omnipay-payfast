8450cd87640e1b267c2bbfe3487e7626bb888488
zodimo <zodimo@gmail.com>
2017-06-12 08:42:13 +0200

    added custom int1..int5 and custom str1..str5

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -42,6 +42,106 @@ class PurchaseRequest extends AbstractRequest
         return $this->setParameter('pdtKey', $value);
     }
 
+    public function getCustomStr1()
+    {
+        return $this->getParameter('customStr1');
+    }
+
+    public function setCustomStr1($value)
+    {
+        return $this->setParameter('customStr1', $value);
+    }
+
+    public function getCustomStr2()
+    {
+        return $this->getParameter('customStr2');
+    }
+
+    public function setCustomStr2($value)
+    {
+        return $this->setParameter('customStr2', $value);
+    }
+
+    public function getCustomStr3()
+    {
+        return $this->getParameter('customStr3');
+    }
+
+    public function setCustomStr3($value)
+    {
+        return $this->setParameter('customStr3', $value);
+    }
+    public function getCustomStr4()
+    {
+        return $this->getParameter('customStr4');
+    }
+
+    public function setCustomStr4($value)
+    {
+        return $this->setParameter('customStr4', $value);
+    }
+
+    public function getCustomStr5()
+    {
+        return $this->getParameter('customStr5');
+    }
+
+    public function setCustomStr5($value)
+    {
+        return $this->setParameter('customStr5', $value);
+    }
+
+    public function getCustomInt1()
+    {
+        return $this->getParameter('customInt1');
+    }
+
+    public function setCustomInt1($value)
+    {
+        return $this->setParameter('customInt1', $value);
+    }
+
+    public function getCustomInt2()
+    {
+        return $this->getParameter('customInt2');
+    }
+
+    public function setCustomInt($value)
+    {
+        return $this->setParameter('customInt2', $value);
+    }
+
+    public function getCustomInt3()
+    {
+        return $this->getParameter('customInt3');
+    }
+
+    public function setCustomInt3($value)
+    {
+        return $this->setParameter('customInt3', $value);
+    }
+
+    public function getCustomInt4()
+    {
+        return $this->getParameter('customInt4');
+    }
+
+    public function setCustomInt4($value)
+    {
+        return $this->setParameter('customInt4', $value);
+    }
+
+    public function getCustomInt5()
+    {
+        return $this->getParameter('customInt5');
+    }
+
+    public function setCustomInt5($value)
+    {
+        return $this->setParameter('customInt5', $value);
+    }
+
+
     public function getData()
     {
         $this->validate('amount', 'description');
@@ -63,7 +163,28 @@ class PurchaseRequest extends AbstractRequest
         $data['amount'] = $this->getAmount();
         $data['item_name'] = $this->getDescription();
 
+        /**
+         *  Custom Ints
+         */
+        $data['custom_int1'] = $this->getCustomInt1();
+        $data['custom_int2'] = $this->getCustomInt2();
+        $data['custom_int3'] = $this->getCustomInt3();
+        $data['custom_int4'] = $this->getCustomInt4();
+        $data['custom_int5'] = $this->getCustomInt5();
+
+        /**
+         * Cutsom strings
+         */
+
+        $data['custom_str1'] = $this->getCustomStr1();
+        $data['custom_str2'] = $this->getCustomStr2();
+        $data['custom_str3'] = $this->getCustomStr3();
+        $data['custom_str4'] = $this->getCustomStr4();
+        $data['custom_str5'] = $this->getCustomStr5();
+
         $data['signature'] = $this->generateSignature($data);
+
+
 
         return $data;
     }
@@ -73,9 +194,33 @@ class PurchaseRequest extends AbstractRequest
         $fields = array();
 
         // specific order required by PayFast
-        foreach (array('merchant_id', 'merchant_key', 'return_url', 'cancel_url', 'notify_url',
-            'name_first', 'name_last', 'email_address', 'm_payment_id', 'amount', 'item_name',
-            'item_description', 'email_confirmation', 'confirmation_address') as $key) {
+        foreach (
+            array(
+                'merchant_id',
+                'merchant_key',
+                'return_url',
+                'cancel_url',
+                'notify_url',
+                'name_first',
+                'name_last',
+                'email_address',
+                'm_payment_id',
+                'amount',
+                'item_name',
+                'item_description',
+                'email_confirmation',
+                'confirmation_address',
+                'custom_int1',
+                'custom_int2',
+                'custom_int3',
+                'custom_int4',
+                'custom_int5',
+                'custom_str1',
+                'custom_str2',
+                'custom_str3',
+                'custom_str4',
+                'custom_str5',
+            ) as $key) {
             if (!empty($data[$key])) {
                 $fields[$key] = $data[$key];
             }

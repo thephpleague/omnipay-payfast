7733d1e4d8a7cb24ee8719ad06498072a41291f7
Jacques Marneweck <jacques@powertrip.co.za>
2018-02-02 14:02:38 +0000

    Reorder and make notes.  Email confirmation needs to be after the custom_xxx items

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -277,10 +277,20 @@ class PurchaseRequest extends AbstractRequest
         // specific order required by PayFast
         // @see https://developers.payfast.co.za/documentation/#checkout-page
         foreach (array('merchant_id', 'merchant_key', 'return_url', 'cancel_url', 'notify_url', 'name_first',
-                     'name_last', 'email_address', 'cell_number', 'm_payment_id', 'amount', 'item_name', 'item_description',
-                     'email_confirmation', 'confirmation_address', 'custom_int1', 'custom_int2', 'custom_int3',
-                     'custom_int4', 'custom_int5', 'custom_str1', 'custom_str2', 'custom_str3', 'custom_str4',
-                     'custom_str5',
+                     'name_last', 'email_address', 'cell_number',
+                    /**
+                     * Transaction Details
+                     */
+                    'm_payment_id', 'amount', 'item_name', 'item_description',
+                    /**
+                     * Custom return data
+                     */
+                    'custom_int1', 'custom_int2', 'custom_int3', 'custom_int4', 'custom_int5',
+                    'custom_str1', 'custom_str2', 'custom_str3', 'custom_str4', 'custom_str5',
+                    /**
+                     * Email confirmation
+                     */
+                    'email_confirmation', 'confirmation_address',
                     /**
                      * Payment Method
                      */
